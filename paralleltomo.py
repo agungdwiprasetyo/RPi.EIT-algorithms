423480e765ed1eec44cf2e2419426c53963d91bb
agungdp22 <agungdwiprasetyo22@gmail.com>
2016-10-17 11:02:14 +0700

    edit kedua

--- paralleltomo.py
+++ paralleltomo.py
@@ -1,7 +1,12 @@
-""" Author : Agung Dwi Prasetyo """
+#!/usr/bin/python3
+
+# """ Author : Agung Dwi Prasetyo """
 
 import numpy as np
+import matplotlib as plot
+import multiprocessing as mp
 
+# untuk fungsi paralel tomo, nilai default yaitu N=80, teta=0 sampai 179, p=None, d=None, isDisp=None
 def paralleltomo(N=80,teta=list(range(0,179)),p=None,d=None,isDisp=0):
     if not p:
         p = round(np.sqrt(2)*N)
@@ -17,6 +22,22 @@ def paralleltomo(N=80,teta=list(range(0,179)),p=None,d=None,isDisp=0):
 
     x = np.arange(-N,N+1)
     y = x
+
+    #
+    rows = np.zeros(2*nA*N*p)
+    cols = rows
+    vals = rows
+    idxend = 0
+
+    if isDisp:
+        # bangkitkan matriks berukuran N*N yag nilai elemennya random
+        AA = np.random.uniform(0,1,N*N) # membangkitkan bilangan random dari 0 sampai 1, masih berupa array 1 dimensi
+        AA = np.reshape(AA,(N,N)) # mengubah array 1 dimensi menjadi array 2 dimensi
+
+    # for i in range(1,nA):
+    #    if isDisp:
+            #
+        #
 
     # masih coba-coba
     A = 1
